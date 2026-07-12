33ccb1c6130b62ab5d38befc71ef0ad7b6ced89b
Tharaka_Madhusanka <tharakalearn@gmail.com>
2026-07-12 12:07:09 +0800

    feat: implement Methrax Spin Wheel component with customizable styles and functionality
    - Added CSS variables for styling the spin wheel in app.css.
    - Updated app.html to include the Methrax Spin Wheel component with default and custom configurations.
    - Enhanced app.ts to manage spin wheel sectors and implement spinRandom functionality.
    - Changed the document title to "Spin Wheel Angular 22" in index.html.
    - Adjusted tsconfig.app.json to extend the correct path for TypeScript configuration.
    - Updated main tsconfig.json to reflect the correct paths for ng-22 project.

--- projects/showcase/ng-17/src/app/app.ts
+++ projects/showcase/ng-17/src/app/app.ts
@@ -1,12 +1,53 @@
-import { Component, signal } from '@angular/core';
+import { Component, QueryList, signal, ViewChild, ViewChildren } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MethraxSpinWheel } from '@methrax/spin-wheel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MethraxSpinWheel],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('ng-17');
+    @ViewChildren('spinwheel') spinwheel = new QueryList<MethraxSpinWheel>();
+
+  sectors = [{
+    id: '1',
+    label: 'One',
+    color : '#1709d9'
+  },
+{
+    id: '2',
+    label: 'Two',
+    color : '#fa4a39'
+  },{
+    id: '3',
+    label: 'Three',
+    color : '#32a852'
+  },
+{
+    id: '4',
+    label: 'Four',
+    color : '#fae039'
+  },
+{
+    id: '5',
+    label: 'Five',
+    color : '#32a852'
+  },
+{
+    id: '6',
+    label: 'Six',
+    color : '#d909d5'
+  },
+{
+    id: '7',
+    label: 'Seven',
+    color : '#9c2f43'
+  }];
+
+  spinRandom() {
+    this.spinwheel.forEach(x => x.spinRandom());
+  }
 }
